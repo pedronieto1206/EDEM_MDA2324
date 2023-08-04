6876bb870a18f6c48eee4761f0b46ec88d9e9784
Pedro Nieto <pedro.nieto@onlyfy.com>
2023-08-04 14:50:07 +0200

    linting changes

--- PetProject/quinielas.py
+++ PetProject/quinielas.py
@@ -1,28 +1,29 @@
-import requests
-import json
-from bs4 import BeautifulSoup
+"""Module for Becoming Rich."""
 import datetime
 import random
-
+import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.eduardolosilla.es/quiniela/ayudas/proximas"
 
 
-page = requests.get(URL)
+page = requests.get(URL,timeout=10)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
 def parse_match(match):
-    partidos={}
-    partidos["local"]=match.split("-")[0]
-    partidos["visitante"]=match.split("-")[1]
-    return partidos
+    """Function parsing match"""
+    part={}
+    part["local"]=match.split("-")[0]
+    part["visitante"]=match.split("-")[1]
+    return part
 
-def calculate_quiniela(quiniela):
-    for partido in quiniela:
-        partido["resultado"]=random.choice(["1","X","2"])
-        quiniela[quiniela.index(partido)]=partido
-    return quiniela
+def calculate_quiniela(qui):
+    """Function parsing quiniela"""
+    for part in qui:
+        part["resultado"]=random.choice(["1","X","2"])
+        qui[qui.index(part)]=part
+    return qui
 
 
 elements=soup.find_all("div", {"class": "c-ayudas-proximas__tabla-partidos ng-star-inserted"})
@@ -38,18 +39,14 @@ for element in elements:
         print("###############################################")
         partidos=element.find_all("span", {"class": "c-equipos__teams m-short ng-star-inserted"})
         pleno=element.find_all("span", {"class": "c-equipos__teams ng-star-inserted"})
-        pleno.text=(pleno[0].text.replace(" ","").replace("\n","-")[1:-1])
+        pleno.text=pleno[0].text.replace(" ","").replace("\n","-")[1:-1]
         partidos.append(pleno)
         for partido in partidos:
             quiniela.append(parse_match(partido.text.replace(" ","").replace("\n","")))
         quiniela=calculate_quiniela(quiniela)
-        print("{:<20} {:<20} {:<10}".format('LOCAL', 'VISITANTE', 'RESULTADO'))
+        print(f'{"string":<20} {"string":<20} {"string":<10}'
+              .format('LOCAL', 'VISITANTE', 'RESULTADO'))
         for element in quiniela:
             local, visitante, resultado = element.values()
-            print("{:<20} {:<20} {:<10}".format(local, visitante, resultado))
-
-
-
-
-
-
+            print(f'{"string":<20} {"string":<20} {"string":<10}'
+                  .format(local, visitante, resultado))
