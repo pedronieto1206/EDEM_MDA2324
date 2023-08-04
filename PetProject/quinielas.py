9a5bade77a862c297b0ed660d9b84f619e6f1c66
Pedro Nieto <pedro.nieto@onlyfy.com>
2023-08-04 14:56:49 +0200

    Update quinielas.py

--- PetProject/quinielas.py
+++ PetProject/quinielas.py
@@ -6,47 +6,53 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.eduardolosilla.es/quiniela/ayudas/proximas"
 
-
-page = requests.get(URL,timeout=10)
+page = requests.get(URL, timeout=10)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
+
 def parse_match(match):
     """Function parsing match"""
-    part={}
-    part["local"]=match.split("-")[0]
-    part["visitante"]=match.split("-")[1]
+    part = {}
+    part["local"] = match.split("-")[0]
+    part["visitante"] = match.split("-")[1]
     return part
+
 
 def calculate_quiniela(qui):
     """Function parsing quiniela"""
     for part in qui:
-        part["resultado"]=random.choice(["1","X","2"])
-        qui[qui.index(part)]=part
+        part["resultado"] = random.choice(["1", "X", "2"])
+        qui[qui.index(part)] = part
     return qui
 
 
-elements=soup.find_all("div", {"class": "c-ayudas-proximas__tabla-partidos ng-star-inserted"})
+elements = soup.find_all(
+    "div", {"class": "c-ayudas-proximas__tabla-partidos ng-star-inserted"})
 
 for element in elements:
-    quiniela=[]
-    jornada=element.find_all("div", {"class": "c-ayudas-proximas__tabla-partidos__titulo"})[0].text
-    fecha=jornada.split("-")[1].replace(" ","")
+    quiniela = []
+    jornada = element.find_all(
+        "div", {"class": "c-ayudas-proximas__tabla-partidos__titulo"})[0].text
+    fecha = jornada.split("-")[1].replace(" ", "")
     date_time_obj = datetime.datetime.strptime(fecha, '%d/%m/%Y')
     if date_time_obj < datetime.datetime.now() + datetime.timedelta(days=7):
         print("###############################################")
         print(jornada)
         print("###############################################")
-        partidos=element.find_all("span", {"class": "c-equipos__teams m-short ng-star-inserted"})
-        pleno=element.find_all("span", {"class": "c-equipos__teams ng-star-inserted"})
-        pleno.text=pleno[0].text.replace(" ","").replace("\n","-")[1:-1]
+        partidos = element.find_all(
+            "span", {"class": "c-equipos__teams m-short ng-star-inserted"})
+        pleno = element.find_all(
+            "span", {"class": "c-equipos__teams ng-star-inserted"})
+        pleno.text = pleno[0].text.replace(" ", "").replace("\n", "-")[1:-1]
         partidos.append(pleno)
         for partido in partidos:
-            quiniela.append(parse_match(partido.text.replace(" ","").replace("\n","")))
-        quiniela=calculate_quiniela(quiniela)
-        print(f'{"string":<20} {"string":<20} {"string":<10}'
-              .format('LOCAL', 'VISITANTE', 'RESULTADO'))
+            quiniela.append(
+                parse_match(partido.text.replace(" ", "").replace("\n", "")))
+        quiniela = calculate_quiniela(quiniela)
+        print(f'{"string":<20} {"string":<20} {"string":<10}'.format(
+            'LOCAL', 'VISITANTE', 'RESULTADO'))
         for element in quiniela:
             local, visitante, resultado = element.values()
-            print(f'{"string":<20} {"string":<20} {"string":<10}'
-                  .format(local, visitante, resultado))
+            print(f'{"string":<20} {"string":<20} {"string":<10}'.format(
+                local, visitante, resultado))
